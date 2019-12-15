d0abc08374b4804c7b9543d9862f48b40f0534d8
Jake Callcut <jakecallcut123@gmail.com>
2019-12-15 15:16:47 +0000

    Create FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import math
+counter = 0
+
+while (counter < 100):
+
+	outputstring = ""
+	counter = counter + 1
+
+	if counter % 3 == 0:
+		outputstring = outputstring + "Fizz"
+	if counter % 5 == 0:
+		outputstring = outputstring + "Buzz"
+
+	if outputstring == "":
+		print(counter)
+	else:
+		print(outputstring)
+
+
+
