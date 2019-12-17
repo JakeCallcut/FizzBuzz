3dd180c2a139f4a22f3a13cff1561c2afad094fa
Jake Callcut <jakecallcut123@gmail.com>
2019-12-17 18:50:36 +0000

    made code smaller for efficiency

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,8 @@
-import sys
-import os
 import math
 counter = 0
-
 while (counter < 100):
-
 	outputstring = ""
 	counter = counter + 1
-
 	if counter % 3 == 0:
 		outputstring = outputstring + "Fizz"
 	if counter % 5 == 0:
